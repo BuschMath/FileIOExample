e7ef3df0b4d809322ef5ff50644452cfb7cb450d
BuschMath <BuschMath@users.noreply.github.com>
2019-09-16 13:46:16 -0500

    Add project files.

---
+++ FileIOExample/Ch4_FileIO_Example.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string data;
+	string data2;
+	string idata3;
+	string data4;
+	string inputFileName = "D:/OneDrive/OneDrive - Iowa Central Community College/Classes/C++/Presentations/Ch4Examples/ExampleInputFile.dat";
+	string outputFileName = "ExampleOutputFile.dat";
+	ifstream inFile;
+	ofstream outFile;
+
+	inFile.open(inputFileName);		// Assumes input file is in the project folder
+	outFile.open(outputFileName);		// Places output file in the project folder
+
+	getline(inFile, data);
+	//inFile >> data >> idata3 >> data2 >> data4;
+	outFile << data;
+
+	cout << data << " " << data2 << " " << idata3 << " " << data4 << endl;
+
+	inFile.close();
+	outFile.close();
+
+	return 0;
+}
